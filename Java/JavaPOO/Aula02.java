6419ae316118b62ca0aaef8d065cc9545106af80
Moises <moisesgithub@gmail.com>
2026-05-04 23:15:23 -0300

    Prática de O que é visibilidade (Java)

--- Java/JavaPOO/Aula02.java
+++ Java/JavaPOO/Aula02.java
@@ -3,18 +3,21 @@ package JavaPOO;
 public class Aula02{
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
+        c1.modelo = "Bic Cristal";
         c1.cor = "Azul";
-        c1.ponta = 0.5f;
+        //c1.ponta = 0.5f;
+        c1.carga = 80;
+        //c1.tampada = true;
         c1.tampar();
-
         c1.status();
         c1.rabiscar();
+        //c1.rabiscar();
 
-        Caneta c2 = new Caneta();
+        /*Caneta c2 = new Caneta();
         c2.modelo = "Hostnet";
         c2.cor = "Preta";
         c2.destampar();
         c2.status();
-        c2.rabiscar();
+        c2.rabiscar();*/
     }
 }
