f29320e1c3eceddda67c09b2b0a2b62aa869c831
Moises <moisesgithub@gmail.com>
2026-05-05 23:12:26 -0300

    praticando Java

---
+++ Java/Desafios/ContadorVogaisConsoantes.java
@@ -0,0 +1,18 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class ContadorVogaisConsoantes {
+    static void main() {
+        Scanner usuario = new Scanner(System.in);
+        String frase;
+        int i, vogais, consoantes;
+        System.out.format("Digite uma frase: ");
+        frase = usuario.next();
+        vogais = 0;
+        consoantes = 0;
+        for (i = 1; i.length(frase)){
+            if (frase, i)
+        }
+    }
+}
