35d5e2f1ae34c5fed2f98c747dad436b9a6d2dfd
moisegit528 <moisesgithub@gmail.com>
2026-05-18 23:48:17 -0300

    Prática (Sistema biblioteca de filmes)

---
+++ Java/Desafios/SistemaBibliotecaFilmes/CadastroFilmes.java
@@ -0,0 +1,26 @@
+package Desafios.SistemaBibliotecaFilmes;
+
+public class CadastroFilmes {
+    private String titulo;
+    private String diretor;
+    private int ano;
+
+    public CadastroFilmes(String titulo, String diretor, int ano){
+        this.titulo = titulo;
+        this.diretor = diretor;
+        this.ano = ano;
+    }
+
+    public void mostrarInfo(){
+        System.out.format("Título: " + titulo);
+        System.out.format("Diretor: " + diretor);
+        System.out.format("Ano do filme: " + ano);
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+}
---
+++ Java/Desafios/SistemaBibliotecaFilmes/Filmes.java
@@ -0,0 +1,12 @@
+package Desafios.SistemaBibliotecaFilmes;
+
+import java.util.Scanner;
+
+public class Filmes {
+    public static void main(String[] args) {
+        Scanner usuario = new Scanner(System.in);
+
+        System.out.println("Quantos filmes deseja cadastra? ");
+        int qtd = usuario.nextInt();
+    }
+}
